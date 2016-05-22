bf89a841c6698dab2cebc619750f6c421f57c68c
jose echavez <joche.echavezm@gmail.com>
2016-05-21 22:24:03 -0500

    update gitignore

--- request.py
+++ request.py
@@ -1,47 +1,57 @@
 import requests
 import hashlib
+import os
 from bs4 import BeautifulSoup
 from parseHtml import * # get parsers functions
 
-SITE_ROOT = 'http://admisiones.unimagdalena.edu.co/mEstudiantes/'
-URL_LOGIN = SITE_ROOT+'/ajaxPages/validarLoginEst.jsp'
-URL_PHOTO = 'http://admisiones.unimagdalena.edu.co/ayreAdmin/mhe/ajaxPages/fa_downFoto.jsp?id='
+# SITE_ROOT = 'http://admisiones.unimagdalena.edu.co/mEstudiantes/'
+# URL_LOGIN = SITE_ROOT+'/ajaxPages/validarLoginEst.jsp'
+# URL_PHOTO = 'http://admisiones.unimagdalena.edu.co/ayreAdmin/mhe/ajaxPages/fa_downFoto.jsp?id='
 
-cod = "2010114113"
-passUn = "andres1082247313"
-password = hashlib.md5(passUn).hexdigest().upper() #encode passs
+cod = "2010114112"
+# passUn = "andres1082247313"
+# password = hashlib.md5(passUn).hexdigest().upper() #encode passs
 
-payload = "user="+cod+"&password="+password
+# payload = "user="+cod+"&password="+password
 
-s = requests.session()
-lg = s.post(URL_LOGIN, params=payload)
+# s = requests.session()
+# lg = s.post(URL_LOGIN, params=payload)
 # print lg.text
 
-def saveFile(name, content):
-    with open(name, 'wf') as file_:
-        file_.write(content.encode('utf8'))
 
-# photo
-tt = s.get(URL_PHOTO+cod)
-with open(cod+'_photo.jpg', 'wb') as f:
-        f.write(tt.content)
+# create folder
+def createFolder(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
 
-# home
-tt = s.get(SITE_ROOT+'inicio.jsp')
-saveFile(cod+'_inicio.html', tt.text)
-proccessHome(tt.text, cod)
+createFolder('res/'+cod)
 
-# schedule
-tt = s.get(SITE_ROOT+'miHorario.jsp')
-HTML_SCHEDULE= tt.text
-saveFile(cod+'_miHorario.html', HTML_SCHEDULE)
 
-# scores
-tt = s.get(SITE_ROOT+'miNotas.jsp')
-HTML_SCORE = tt.text
-saveFile(cod+'_miNotas.html', HTML_SCORE)
+# def saveFile(name, content):
+    # with open(name, 'wf') as file_:
+        # file_.write(content.encode('utf8'))
 
-# bedsheet
-tt = s.get(SITE_ROOT+'miSabana.jsp')
-HTML_BEDSHEET = tt.text
-saveFile(cod+'_miSabana.html', HTML_BEDSHEET)
+# # photo
+# tt = s.get(URL_PHOTO+cod)
+# with open(cod+'_photo.jpg', 'wb') as f:
+        # f.write(tt.content)
+
+# # home
+# tt = s.get(SITE_ROOT+'inicio.jsp')
+# saveFile(cod+'_inicio.html', tt.text)
+# proccessHome(tt.text, cod)
+
+# # schedule
+# tt = s.get(SITE_ROOT+'miHorario.jsp')
+# HTML_SCHEDULE= tt.text
+# saveFile(cod+'_miHorario.html', HTML_SCHEDULE)
+
+# # scores
+# tt = s.get(SITE_ROOT+'miNotas.jsp')
+# HTML_SCORE = tt.text
+# saveFile(cod+'_miNotas.html', HTML_SCORE)
+
+# # bedsheet
+# tt = s.get(SITE_ROOT+'miSabana.jsp')
+# HTML_BEDSHEET = tt.text
+# saveFile(cod+'_miSabana.html', HTML_BEDSHEET)

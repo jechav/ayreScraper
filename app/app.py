6bd5891bd7c5787d0fe542f0b0d8073dc278dee9
jochechavez <joche.echavezm@gmail.com>
2016-06-04 02:49:26 -0400

    deploy config

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@
 from flask import Flask, jsonify, request, make_response
 from requestAyre import main_request # get parsers functions
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 def r_error(err):
     if err == 400:
@@ -10,11 +10,11 @@ def r_error(err):
     else:
         return make_response(jsonify({'error': 'Bad request'}), err)
 
-@app.route('/')
+@application.route('/')
 def index():
     return "Ayre Scraper"+" make request to '/ayre/api/v1/auth/' with params cod, password"
 
-@app.route('/ayre/api/v1/auth/', methods=['POST'])
+@application.route('/ayre/api/v1/auth/', methods=['POST'])
 def auth():
     if not request.form or not 'cod' in request.form or not 'password' in request.form:
         return r_error(500)
@@ -24,9 +24,9 @@ def auth():
 
 
 
-# @app.route('/ayre/api/v1/task/', methods=['GET'])
+# @application.route('/ayre/api/v1/task/', methods=['GET'])
 # def getTasks():
     # return jsonify({'tasks': tasks})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(host='0.0.0.0')

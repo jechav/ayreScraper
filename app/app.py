a182784ddce78ad0aacaf2aa00663eb3a64108f3
jose echavez <joche.echavezm@gmail.com>
2016-06-05 13:56:04 -0500

    valid errors

--- app/app.py
+++ app/app.py
@@ -6,24 +6,23 @@ application = Flask(__name__)
 application.debug = True
 
 def r_error(err):
-    if err == 400:
-        return make_response(jsonify({'error': 'No found'}), err)
-    else:
-        return make_response(jsonify({'error': 'Bad request'}), err)
+        return make_response(jsonify({'error': err}), 400)
 
 @application.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         if not request.form or not 'cod' in request.form or not 'password' in request.form:
-            return r_error(500)
+            return r_error('Bad request')
         else:
-            return jsonify( main_request(request.form['cod'], request.form['password']) )
+            res = main_request(request.form['cod'], request.form['password'])
+            if isinstance(res, dict):
+                return jsonify( res )
+            else:
+                return r_error(res);
+
     else:
         return render_template('index.html');
 
-# @application.route('/ayre/api/v1/task/', methods=['GET'])
-# def getTasks():
-    # return jsonify({'tasks': tasks})
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0')
--- app/static/js/app/app.js
+++ app/static/js/app/app.js
@@ -10,8 +10,8 @@ angular.module('app', [])
   //$scope.user.cod = 2014115021;
   //$scope.user.password = 'nacielguillermo123';
 
-  $scope.user.cod = 2010114040;
-  $scope.user.password = 'jochechavez123';
+  //$scope.user.cod = 2010114040;
+  //$scope.user.password = 'jochechavez123';
 
   $scope.busy = false;
   $scope.results = null;
@@ -30,7 +30,7 @@ angular.module('app', [])
     }
     function error(res){
       console.warn(res);
-      alert( JSON.stringify(res.data) );
+      alert( JSON.stringify(res.data.error) );
       $scope.busy = false;
     }
 
